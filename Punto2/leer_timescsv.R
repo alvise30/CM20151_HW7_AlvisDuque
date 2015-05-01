3b5e75c32d3b4c705cadcfefc2f1db5b78272cc0
alvise <elkinalvisnarvaez@gmail.com>
2015-04-30 22:04:08 -0500

    parece ser la gráfica

--- Punto2/leer_timescsv.R
+++ Punto2/leer_timescsv.R
@@ -7,13 +7,13 @@ times <- read.csv('times.csv')
 date <- times[,2]
 
 #Creamos un vector para guardar las diferencias de tiempos obtenidas
-x <- c(NULL)
+x <- c('Intervalo',45.0)
 #Calculamos todas las diferencias de tiempo
 for (i in 2:length(date)) {
   tmp <- strptime(date[i], format="%Y.%m.%d_%H:%M:%S")-strptime(date[i-1], format="%Y.%m.%d_%H:%M:%S")
   x <- c(x,tmp)
 }
-
+#x <- c(x,45.0)
 #Escribimos un .txt para guardar los datos
 fout<-file("intervalos.txt")
 write(x, fout, sep = '\n')
