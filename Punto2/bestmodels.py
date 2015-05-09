c5bcdc167a97094e6f52a5b3b8ddc0b711459357
alvise <elkinalvisnarvaez@gmail.com>
2015-05-08 19:28:58 -0500

    imagenes lestas

--- Punto2/bestmodels.py
+++ Punto2/bestmodels.py
@@ -131,53 +131,52 @@ k=0
 i = 0
 j = 0
 for element in tmp:   
-    if element == 1:
-        #figsize(8,5.5)
+    if element == 1:#Para lineal
         best_y = linear_model(x_obs, bestm.iloc[k+0,2], bestm.iloc[k+1,2])
         scatter(x_obs,y_obs[:,i,j], color = 'grey', label = (u'Observacion'))
-        plot(x_obs, best_y, 'b', label = (u'Ajuste Lineal'))
-        legend(loc=4, fontsize=12)
+        plot(x_obs, best_y, 'b', label = (u'Ajuste Lineal' +'\n'+ r'$at+b$' + '\na--> '+str(bestm.iloc[k+0,2]) 
+                                          + '\nb--> ' + str(bestm.iloc[k+1,2])+ '\nLikelihood--> ' 
+                                          + str(bestm.iloc[k+2,2])))
+        legend(loc=(1.03,0.2), fontsize=15)
         xlabel(r'$Tiempo\ [horas]$', fontsize=16)
         ylabel(r'$Campo\ Magn\'etico\ [Gauss]$', fontsize=16)
-        title(u'Observacion_' +str(i)+ '_' +str(j), fontsize=24)
-        text(0,10, 'a= '+str(bestm.iloc[k+0,2]) + '\nb= '+str(bestm.iloc[k+1,2]) + '\nLikelihood= ' 
-             +str(bestm.iloc[k+2,2]), fontsize=12)
-        text(0,0, r'$at+b$', fontsize=24)
-        savefig(u'Observacion_'+str(i)+'_'+str(j)+'.pdf')
+        title(u'Observacion_' +str(i+138)+ '_' +str(j+241), fontsize=24)
+        savefig(u'Observacion_'+str(i+138)+'_'+str(j+241)+'.pdf', bbox_inches='tight')
         close()
         k+=3        
-        print 'lineal',i,j
-    elif element == 2:
-       # plt.figsize(8,5.5)
+        print 'lineal',i+138,j+241
+
+    elif element == 2:#Para Gaussiano
         best_y = linear_gauss_model(x_obs, bestm.iloc[k+0,2], bestm.iloc[k+1,2], bestm.iloc[k+2,2], bestm.iloc[k+3,2])
         scatter(x_obs,y_obs[:,i,j], color ='grey', label = u'Observacion')
-        plot(x_obs, best_y, 'm', label = u'Ajuste Lineal Gaussiano')
-        legend(loc=2)
+        plot(x_obs, best_y, 'm', label = (u'Ajuste Lineal Gaussiano' + '\n' + r'$a+bt+\frac{1}{c\sqrt{\pi}}e^{-\frac{1}{2}[\frac{t-d}{c}]^2}$'
+                                          + '\na--> '+str(bestm.iloc[k+0,2]) + '\nb--> '
+                                          + str(bestm.iloc[k+1,2])   + '\nc--> '+str(bestm.iloc[k+2,2]) + '\nd--> ' 
+                                          + str(bestm.iloc[k+3,2])   + '\nLikelihood--> ' + str(bestm.iloc[k+4,2])))
+        legend(loc=(1.03,0.2), fontsize=15)
         xlabel(r'$Tiempo\ [horas]$', fontsize=16)
         ylabel(r'$Campo\ Magn\'etico\ [Gauss]$', fontsize=16)
-        title(u'Observacion_' +str(i)+ '_' +str(j), fontsize=24)
-        text(4.5,-15, 'a= '+str(bestm.iloc[k+0,2]) + '\nb= '+str(bestm.iloc[k+1,2]) + '\nc= '+str(bestm.iloc[k+2,2]) + 
-             '\nd= '+str(bestm.iloc[k+3,2]) + '\nLikelihood= ' +str(bestm.iloc[k+4,2]), fontsize=12)
-        text(-0.5,5, r'$a+bt+\frac{1}{c\sqrt{\pi}}e^{-\frac{1}{2}[\frac{t-d}{c}]^2}$', fontsize=24)
-        savefig(u'Observacion_'+str(i)+'_'+str(j)+'.pdf')
+        title(u'Observacion_' +str(i+138)+ '_' +str(j+241), fontsize=24)
+        savefig(u'Observacion_'+str(i+138)+'_'+str(j+241)+'.pdf', bbox_inches='tight')
         close()
         k+=5                
-        print 'gauss',i,j
-    elif element == 3:
-        #figsize(8,5.5)
+        print 'gauss',i+138,j+241
+
+    elif element == 3:#Para paso
         best_y = linear_paso_model(x_obs, bestm.iloc[k+0,2], bestm.iloc[k+1,2], bestm.iloc[k+2,2], bestm.iloc[k+3,2], bestm.iloc[k+4,2])
         scatter(x_obs,y_obs[:,i,j], color = 'grey', label = (u'Observacion'))
-        plot(x_obs, best_y, 'g', label = (u'Ajuste Lineal Paso'))
-        legend(loc=2, fontsize=12)
+        plot(x_obs, best_y, 'g', label = (u'Ajuste Lineal Paso' + '\n' + r'$a+bt+c[1+\frac{2}{\pi}arctan(d(t-e))]$'
+                                          + '\na--> '+str(bestm.iloc[k+0,2]) + '\nb--> '
+                                          + str(bestm.iloc[k+1,2]) + '\nc--> '+str(bestm.iloc[k+2,2]) + '\nd--> ' 
+                                          + str(bestm.iloc[k+3,2]) + '\ne--> '  +str(bestm.iloc[k+4,2]) + '\nLikelihood--> ' 
+                                          + str(bestm.iloc[k+5,2])))
+        legend(loc=(1.03,0.2), fontsize=15)
         xlabel(r'$Tiempo\ [horas]$', fontsize=16)
         ylabel(r'$Campo\ Magn\'etico\ [Gauss]$', fontsize=16)
-        title(u'Observacion_' +str(i)+ '_' +str(j), fontsize=24)
-        text(4.5,-15, 'a= '+str(bestm.iloc[k+0,2]) + '\nb= '+str(bestm.iloc[k+1,2]) + '\nc= '+str(bestm.iloc[k+2,2]) + 
-             '\nd= '+str(bestm.iloc[k+3,2]) + '\ne= '+str(bestm.iloc[k+4,2]) + '\nLikelihood= ' +str(bestm.iloc[k+5,2]), fontsize=12)
-        text(-0.5,5, r'$a+bt+c[1+\frac{2}{\pi}arctan(d(t-e))]$', fontsize=20)
-        savefig(u'Observacion_'+str(i)+'_'+str(j)+'.pdf')
+        title(u'Observacion_' +str(i+138)+ '_' +str(j+241), fontsize=24)
+        savefig(u'Observacion_'+str(i+138)+'_'+str(j+241)+'.pdf', bbox_inches='tight')
         close()        
         k+=6        
-        print 'paso',i,j
+        print 'paso',i+138,j+241
         
     i, j = comprobar(i, j, len(tmp)/2)
